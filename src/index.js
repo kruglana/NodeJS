669cc2f3b983e079e01f6765695aa7a9597333f0
kruglana <n24s22@mail.ru>
2016-11-21 15:44:05 +0300

    2B

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
-app.get('/taskMy2', (req, res) =>{
+app.get('/taskMy2B', (req, res) =>{
   const fio = req.query.fullname;
   try{
     var answer;
-    var newStr = fio.split(' ');
+    var newStr = fio.replace(/\s\s+/g, ' ').trim().split(' ');
 
     if(newStr.length > 3 || newStr[0] == '' ||
-       fio.match(/\d/)!=null) {
+       fio.match(/\d/)!=null || fio.match(/[_\/]/)!=null) {
       answer = 'Invalid fullname';
     }
     else{
-      answer = newStr[newStr.length-1];
+      answer = newStr[newStr.length-1][0].toUpperCase() + newStr[newStr.length-1].substr(1).toLowerCase();
       for(var i=0;i<newStr.length-1;i++) {
-        answer += ' ' + newStr[i].substring(0,1) + '.';
+        answer += ' ' + newStr[i][0].toUpperCase() + '.';
       }
    }
    res.send(answer.toString());
